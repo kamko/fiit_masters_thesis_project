7402f1438f8e0a88409b2f0c4f516539f092bb44
Kamil Janeček <j@necek.com>
2019-10-10 16:51:20 +0200

    [monant-sync] add schema skeleton

---
+++ monant-sync/sync/db/db.py
@@ -0,0 +1,64 @@
+from sqlalchemy import create_engine
+from sqlalchemy.schema import Column, ForeignKey, Table
+from sqlalchemy.orm import relationship
+from sqlalchemy.types import Text, BigInteger, DateTime
+from sqlalchemy.dialects.postgresql import JSONB
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+media_article_table = Table('article_media',
+                            Base.metadata,
+                            Column('article_id', BigInteger,
+                                   ForeignKey('article.id')),
+                            Column('media_id', BigInteger,
+                                   ForeignKey('media.id')))
+
+
+class Article(Base):
+    __tablename__ = 'article'
+
+    id = Column(BigInteger, primary_key=True)
+
+    title = Column(Text)
+    perex = Column(Text)
+    body = Column(Text)
+    published_at = Column(DateTime)
+    url = Column(Text)
+
+    source_id = Column(BigInteger)
+    source = relationship("Source", uselist=False)
+
+    media = relationship("Media", secondary=media_article_table)
+
+    category = Column(Text)
+
+    other_info = Column(JSONB)
+
+
+class Source(Base):
+    __tablename__ = 'source'
+
+    id = Column(BigInteger, primary_key=True)
+
+    name = Column(Text)
+    url = Column(Text)
+
+
+class Media(Base):
+    __tablename__ = 'media'
+
+    id = Column(BigInteger, primary_key=True)
+
+    caption = Column(Text)
+    media_type = Column(Text)
+    url = Column(Text)
+
+
+def engine():
+    from os import environ
+    return create_engine(os.environ['POSTGRESQL_URI'])
+
+
+def create_all_tables(engine):
+    Base.metadata.create_all(engine)
---
+++ monant-sync/sync/db/mapper.py
@@ -0,0 +1,8 @@
+def map_article(article):
+    pass
+
+def map_source(source):
+    pass
+
+def map_media(media):
+    pass
