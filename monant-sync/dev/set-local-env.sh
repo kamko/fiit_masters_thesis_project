960eea2d7e51ab7056eb9358f6082154921a9abd
Kamil Janeček <j@necek.com>
2019-10-10 23:58:12 +0200

    [monant-sync] move dev scripts

--- monant-sync/dev/set-local-env.sh
+++ monant-sync/dev/set-local-env.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
+set -e
+
 auth_file=$1
 
 if [[ -z "$auth_file" ]]; then echo "Missing 'auth_file' parameter" && exit 1; fi
 
-auth=$(sops -d $auth_file)
+auth=$(cat $auth_file)
 
 while read -r line
     do 
