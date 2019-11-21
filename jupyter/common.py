5726c3d8d27cc4868eab5f3bd6639f960386f613
Kamil Janeček <j@necek.com>
2019-11-21 16:15:36 +0100

    [jupyter] periodic save

--- jupyter/common.py
+++ jupyter/common.py
@@ -1,3 +1,4 @@
+from contextlib import contextmanager
 def create_engine(conf_file, key):
     import json
     from sqlalchemy import create_engine
@@ -6,7 +7,16 @@ def create_engine(conf_file, key):
         conf = json.load(f)
         return create_engine(conf[key]['uri'])
 
+    
 def display_all(df):
     import pandas as pd
     with pd.option_context('display.max_rows', None, 'display.max_columns', None):
         display(df)
+
+@contextmanager
+def figsize(plt, x, y):
+    try:
+        plt.rcParams['figure.figsize'] = (x, y)
+        yield
+    finally:
+        plt.rcParams['figure.figsize'] = (6.4, 4.8)
