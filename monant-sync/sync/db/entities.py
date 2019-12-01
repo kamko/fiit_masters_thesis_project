8902438e4c6cb7b3bee7c1359c3a63280be01dde
Kamil Janeček <j@necek.com>
2019-12-01 16:34:03 +0100

    [monant-sync] update schema 2

--- monant-sync/sync/db/entities.py
+++ monant-sync/sync/db/entities.py
@@ -32,10 +32,10 @@ class Article(Base):
     source = relationship("Source", uselist=False)
 
     author_id = Column(BigInteger, ForeignKey('author.id'))
-    author = relationship("Author", uselist=False, cascade='save-update')
+    author = relationship("Author", uselist=False)
 
     media = relationship(
-        'Media', secondary=media_article_table, cascade='save-update')
+        'Media', secondary=media_article_table)
 
     fb_engagement = relationship('FacebookEngagement', uselist=False)
 
