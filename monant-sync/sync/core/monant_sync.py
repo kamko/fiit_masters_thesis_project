9118ec9958e9f2470ae227a80172010b74fad543
Kamil Janeček <j@necek.com>
2019-12-03 12:05:10 +0100

    [monant-sync] workaround 2

--- monant-sync/sync/core/monant_sync.py
+++ monant-sync/sync/core/monant_sync.py
@@ -97,9 +97,9 @@ def fetch_all_articles(api_client):
         print(f'batch_no={i}')
         batch = [map_article(a) for a in batch]
         with get_engine().begin() as engine:
-            engine.execute(insert(Source).on_conflict_do_nothing(),
+            engine.execute(insert(Source).on_conflict_do_update(),
                            [art.source.__dict__ for art in batch if art.source is not None])
-            engine.execute(insert(Author).on_conflict_do_nothing(),
+            engine.execute(insert(Author).on_conflict_do_update(),
                            [art.author.__dict__ for art in batch if art.author is not None])
         
         _save_all(batch, merge=True)
