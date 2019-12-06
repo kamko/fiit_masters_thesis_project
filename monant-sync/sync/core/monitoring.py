411d712e736f209b21521d910e9ee36ae7c166f3
Kamil Janeček <j@necek.com>
2019-12-06 13:07:44 +0100

    [monant-sync] add treshold for monitoring

--- monant-sync/sync/core/monitoring.py
+++ monant-sync/sync/core/monitoring.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+import datetime as dt
 
 from .monant_sync import new_articles_iterator, sources_iterator
 from .mapper import map_article, map_engagement, map_source
@@ -9,9 +9,10 @@ from util import flatten_iterable, chunks
 
 from sqlalchemy import func
 
-date_threshold = datetime(2019, 10, 21) 
+date_threshold = dt.datetime(2019, 10, 21, 0, 0, 0, 0, tzinfo=dt.timezone(dt.timedelta(seconds=3600)))
 
-def _should_fetch_new_articles(session):    
+
+def _should_fetch_new_articles(session):
     total_monitored = session.query(func.count(MonitoredArticle.id)) \
         .scalar()
 
@@ -19,32 +20,32 @@ def _should_fetch_new_articles(session):
 
 
 def _refresh_sources_list(session, monant_client):
-    print(f'[_refresh_sources_list - {datetime.now()}]')
+    print(f'[_refresh_sources_list - {dt.datetime.now()}]')
     for source in map(map_source, flatten_iterable(sources_iterator(monant_client))):
         session.merge(source)
 
 
 def _fetch_new_articles(session, monant_client):
-    print(f'[_fetch_new_articles - {datetime.now()}]')
+    print(f'[_fetch_new_articles - {dt.datetime.now()}]')
     last_id = run_action('last-id')
     it = flatten_iterable(new_articles_iterator(monant_client, last_id, 500))
 
     res = []
     for api_article in it:
         article = map_article(api_article)
-        
+
         article.source = merge_if_not_none(session, article.source)
         article.author = merge_if_not_none(session, article.author)
         session.add(article)
 
-        if datetime.strptime(article.published_at, '%Y-%m-%dT%H:%M:%S.%f%z') > date_threshold:
+        if dt.datetime.strptime(article.published_at, '%Y-%m-%dT%H:%M:%S.%f%z') > date_threshold:
             res.append(article)
 
     return res
 
 
 def _mark_articles_as_monitored(session, articles):
-    print(f'[_mark_articles_as_monitored - {datetime.now()}]')
+    print(f'[_mark_articles_as_monitored - {dt.datetime.now()}]')
     for article in articles:
         monitored_article = MonitoredArticle(article)
         session.add(monitored_article)
@@ -52,7 +53,7 @@ def _mark_articles_as_monitored(session, articles):
 
 
 def _fetch_engagement_for_monitored_articles(session, fb_client):
-    print(f'[_fetch_engagement_for_monitored_articles - {datetime.now()}]')
+    print(f'[_fetch_engagement_for_monitored_articles - {dt.datetime.now()}]')
     mas = session.query(MonitoredArticle) \
         .order_by(MonitoredArticle.article_id.asc()) \
         .all()
@@ -78,7 +79,7 @@ def _fetch_engagement_for_monitored_articles(session, fb_client):
 
 def run(monant_client_provider, fb_client_provider):
     with session_scope() as session:
-        start_time = datetime.now()
+        start_time = dt.datetime.now()
         print(f'[monitor] started at {start_time}')
 
         try:
@@ -88,10 +89,10 @@ def run(monant_client_provider, fb_client_provider):
                 articles = _fetch_new_articles(session, monant_client)
                 _mark_articles_as_monitored(session, articles)
         except ConnectionError as err:
-            print(f'[monitor] - monant fetch failed! reason={err}')        
+            print(f'[monitor] - monant fetch failed! reason={err}')
         total = _fetch_engagement_for_monitored_articles(
             session, fb_client=fb_client_provider())
 
-        end_time = datetime.now()
+        end_time = dt.datetime.now()
         print(f'[monitor] finished at {end_time}')
         session.add(MonitorJobRunLog(start_time, end_time, total))
